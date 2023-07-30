59659d901afa869e3e4f5983912acb056aa18a3e
keixjk <kayegallogo@gmail.com>
2023-07-30 18:37:29 +0800

    def for speed, on, radius and color

--- classFile.py
+++ classFile.py
@@ -3,3 +3,9 @@ class Fan:
     SLOW = 1
     MEDIUM = 2
     FAST = 3
+
+    def __init__(self, speed=SLOW, radius=5, color='blue', on=False):
+        self.__speed = speed
+        self.__on = on
+        self.__radius = radius
+        self.__color = color
